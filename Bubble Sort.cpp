143f528a0ca3c9332f472fbc588c3bb26b8156b6
only-hub <99th_day@naver.com>
2018-04-05 17:43:05 +0900

    버블정렬
    Bubble Sort

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -8,7 +8,7 @@ for (loop = 0; loop < tempCount - 1; loop++ ){
       hold = temp[i];
       temp[i] = temp[i+1];
 // 가장 큰 원소는 정렬이 됐고, 그 것을 제외한 나머지를 반복
-      temp[i+1] = hold;
+      temp[i+1] = hold; 
     }
   }
 }
