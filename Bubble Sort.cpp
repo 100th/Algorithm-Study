352f7783686f27d9604cb8105fb087ff227f4c21
only-hub <99th_day@naver.com>
2018-04-05 17:31:38 +0900

    버블 정렬

---
+++ Bubble Sort.cpp
@@ -0,0 +1,14 @@
+//tempCount : 배열의 크기
+for (loop = 0; loop < tempCount - 1; loop++ ){
+// 배열의 원소 크기만큼 반복한다.
+  for (i = 0; i < tempCount - 1 - loop; i++){
+    //인접한 원소가 작으면 교환한다.
+    if (temp[i] > temp[i+1])
+    {
+      hold = temp[i];
+      temp[i] = temp[i+1];
+// 가장 큰 원소는 정렬이 됐고, 그 것을 제외한 나머지를 반복
+      temp[i+1] = hold;
+    }
+  }
+}
